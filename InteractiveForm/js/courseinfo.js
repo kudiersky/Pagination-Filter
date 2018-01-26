7fd9c9c45ba1cb607d4b033ec6f585981610ca91
kudiersky <32683429+kudiersky@users.noreply.github.com>
2018-01-26 16:22:40 +0000

    validation v3

---
+++ InteractiveForm/js/courseinfo.js
@@ -0,0 +1,26 @@
+var courses = [
+  {
+    name: "Main Conference ",
+    cost: 200,
+  },
+  {
+    name: "JavaScript Frameworks Workshop",
+    cost: 200
+  },
+  {
+    name: "Express Workshop",
+    cost: 100
+  },
+  {
+    name: "Node.js Workshop",
+    cost: 100,
+  },
+  {
+    name: "Build tools Workshop",
+    cost: 100
+  },
+  {
+    name: "npm Workshop",
+    cost: 100
+  }
+];
